fb10057a68de04743ff438aca568f6277ff0f56e
Lucasleallopes <lucasrodrigoleallopes2014@gmail.com>
2024-10-26 13:00:42 -0300

    fix: correcoes gerais e implementacao cliente

--- src/pages/Clientes.tsx
+++ src/pages/Clientes.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import ListClientes from '../components/ListClientes'
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
+import { useAuth } from '@/context/AuthContext';
 
 const Clientes: React.FC = () => {
+  const { user } = useAuth();
+
   return (
     <div className="p-6">
-      <h1>Clientes</h1>
-      <p>Isso é definitivamente um texto para preencher espaço</p>
+      <h1 className="text-2xl font-bold mb-4">Gerenciamento de Clientes</h1>
+
+      {user && user.role === 'admin' && (
+        <div className="mb-4">
+          <Link to="/clientes/new">
+            <Button>Adicionar Cliente</Button>
+          </Link>
+        </div>
+      )}
+      <ListClientes />
     </div>
   );
 };
